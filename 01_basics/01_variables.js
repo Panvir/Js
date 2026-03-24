1cf23d043834363d399f80ae2839228d7f4be0b4
Panvir <panvirsingh2002@gmail.com>
2026-03-25 00:31:35 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail= "panvirsingh2202@gmail.com"
+var accountPassword= "12345"
+accountCity="jaipur" //valid hai but dont use it
+let accountState;
+
+// accountId=2 /not allowed in case of const
+
+/*
+prefer not to use var 
+because og issie of blocl scope and fxn scope
+*/
+
+accountEmail="hdfc@gmail.com"
+accountPassword="93848"
+console.log(accountId);
+console.table([accountId,accountPassword,accountCity,accountEmail,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3) ;
+console.log("hitesh")
+
+let name="hitesh"
+let age= 3
+let isloggedIn=true
+
+
+//number =>2 to power 53
+//bigint vale nhi use krte but stock market ma krte hai use
+//string sinhle qoutes better is double qoutes
+//Boolean->true/false
+//null ye datatype bhi hai ye standlone value hai -- ye bs representation hai empty nu represent kreha hai
+//undefined=> jaise va;ue define na kri ho 
+//symbol
+
+//objects
+
+console.log(typeof "hitesh")
+console.log(typeof undefined)// op is undefined
+// null ka objet ata hai type
+
