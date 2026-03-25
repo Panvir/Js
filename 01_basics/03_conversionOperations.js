d385449216d8db57963974689aa1b6abb7efb164
Panvir <panvirsingh2002@gmail.com>
2026-03-25 11:30:49 +0530

    conversion of datatyoes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score=true
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber=Number(score)//type cast
+console.log(typeof(valueInNumber));
+console.log(valueInNumber) //outupt is Not a number ie NaN
+
+//"33" can be coverted to number
+//33abc cant be so NaN
+// true->1 false->0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIN=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIN)
+//1->true,0->false
+//hitesh->true
+let someNumber= 33
+let stringNumber=String(someNumber);
+console.log(stringNumber);console.log(typeof stringNumber);
+
